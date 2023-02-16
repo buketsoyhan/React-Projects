223a113a16c299f5735f3d47cc10fedc6c513a06
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-02-17 02:48:10 +0300

    Connection established with auth0

--- user-login-auth/src/App.js
+++ user-login-auth/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+// import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
   return (
-    <div className="App">
-      asdas
+    <div >
+      <Login/>
+      <Logout/>
+      <Profile/>
     </div>
   );
 }
