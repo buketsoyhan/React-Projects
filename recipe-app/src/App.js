334a61f12dd46a1c135eb58e33c357191c97fe9d
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-02-18 21:08:06 +0300

    Used some hooks

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "ba6241ee";
+  const APP_KEY = "fc505d609b490ef95aae07d7d4138f62";
+  const exReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  }, []);
+
   return (
     <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div>
+        <button onClick={increment}>Click Here</button>
+        {counter}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
